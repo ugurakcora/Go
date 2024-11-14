01d65f7e90f66d5e7883f340145f69d7a2acef32
ugurakcora <uakcora97@gmail.com>
2024-11-15 02:13:25 +0300

    feat: helloworld

---
+++ hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+		fmt.Println("Hello, World!")
+}
+
+// package main nedir? : main paketi, bir uygulamanın çalıştırılabilir olduğunu belirtir.
+// import "fmt" nedir? : fmt paketini içeri aktarır. fmt paketi, biçimlendirilmiş giriş ve çıkış sağlar.
+// func main() nedir? : main() fonksiyonu, bir uygulamanın başlangıç noktasıdır. Bir uygulama çalıştırıldığında, main() fonksiyonu otomatik olarak çalıştırılır.
+// fmt.Println() nedir? : fmt.Println() fonksiyonu, bir dizeyi konsola yazdırmak için kullanılır.
+// go run main.go komutu ile çalıştırılır.
+// go build main.go komutu ile çalıştırılabilir dosya oluşturulur.
